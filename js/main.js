c3a7327a151e9c9bcd8253da58b85e7c615f6b87
philgm <philippe.marchal@ifsttar.fr>
2013-12-17 22:08:15 +0100

    et encore deux http

--- js/main.js
+++ js/main.js
@@ -34,7 +34,7 @@ $(function () {
     if (window.location.hostname === 'blueimp.github.io') {
         // Demo settings:
         $('#fileupload').fileupload('option', {
-            url: '//jquery-file-upload.appspot.com/',
+            url: 'http://jquery-file-upload.appspot.com/',
             // Enable image resizing, except for Android and Opera,
             // which actually support image resizing, but fail to
             // send Blob objects via XHR requests:
@@ -46,7 +46,7 @@ $(function () {
         // Upload server status check for browsers with CORS support:
         if ($.support.cors) {
             $.ajax({
-                url: '//jquery-file-upload.appspot.com/',
+                url: 'http://jquery-file-upload.appspot.com/',
                 type: 'HEAD'
             }).fail(function () {
                 $('<div class="alert alert-danger"/>')
